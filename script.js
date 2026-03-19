53a4304ef001bff891dc680d2384b182d40fe317
murali-krishna-132 <murali.k@somaiya.edu>
2026-03-19 17:01:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function calculateSI(){
+
+let p = document.getElementById("principal").value;
+let r = document.getElementById("rate").value;
+let t = document.getElementById("time").value;
+
+let si = (p * r * t) / 100;
+
+document.getElementById("result").innerText =
+"Simple Interest = " + si;
+
+}
